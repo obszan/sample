f58b78081bfd636610004b9c0aeed028874be47e
Mateusz Obszanski <mos@trackman.com>
2022-07-15 19:53:20 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'>netlify test</div>;
+  return <div className='App'>netlify test 123 123 </div>;
 }
 
 export default App;
